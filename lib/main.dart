46a8fd072d7987b49c44414a48e1ef25ef94201c
Bobby Ishak <bobbyishak@yahoo.com>
2023-04-01 14:12:18 +0700

    Convert to stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,14 @@ void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
